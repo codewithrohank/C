57f6fc5d8484faf415f80ee82c41d6eee71d18fc
codewithrohank <rohankavade91@gmail.com>
2021-05-19 16:54:34 +0530

    printf-scanf example

---
+++ Input-Output-Functions/printf-scanf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+    /* Declare an integer */
+    int i;
+
+    printf("Enter a Number\n");
+    /*
+    ** scanf() will wait for input. Enter a number
+    ** as input and that will be stored in 'i'.
+    ** NOTE : scanf needs an address of 'i' to 
+    ** store the value on that address.
+    */
+    scanf("%d", &i);
+
+    /*
+    ** printf() will display the number
+    ** stored in 'i'.
+    */
+    printf("You have entered the Number:%d\n", i);
+
+    return 0; 
+}
