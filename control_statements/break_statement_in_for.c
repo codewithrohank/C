89c2441b0b625133e65a41153cc11d1e8af6dfa2
codewithrohank <rohankavade91@gmail.com>
2021-06-06 12:03:31 +0530

    update

---
+++ control_statements/break_statement_in_for.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    printf("Code with Rohan\n");
+
+    for(i = 0; i < 3; i++)
+    {
+        printf("Like - %d\n", i);
+        break;
+    }
+    return 0;
+}
+
