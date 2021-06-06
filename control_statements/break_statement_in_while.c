9fdf4d4b86c1dc2fec119e9c60fd2bf817b82503
codewithrohank <rohankavade91@gmail.com>
2021-06-06 12:01:55 +0530

    break statement programs

---
+++ control_statements/break_statement_in_while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 0;
+    printf("Code With Rohan\n");
+
+    while (num < 3)
+    {
+        printf("Like - %d\n", num);
+        break;
+        num++;
+    }
+
+}
+
+
