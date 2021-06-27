3228fc9583b404b43b73e834dd322c6c2b57befa
codewithrohank <rohankavade91@gmail.com>
2021-06-27 12:25:54 +0530

    array update

--- array/array_quiz-4.c
+++ array/array_quiz-4.c
@@ -5,6 +5,11 @@ int main()
     char arr1[] = "abcd";
     char arr2[] = "abcd";
 
+    printf("%u, %u\n", arr1, arr2);
+    printf("%u, %u\n", &arr1, &arr2);
+    printf("%s, %s\n", arr1, arr2);
+    printf("%u, %u\n", sizeof(arr1), sizeof(arr2));
+
     if (arr1 == arr2)
         printf("Equal\n");
     else
